c825be043cb1ebb5d8b1f4f0a21ff71747c7d2a4
yifan <yifanxiazzz@gmail.com>
2026-09-15 20:13:19 +0800

    Round73: complete shared startup admission and preserve second preflight failure

--- src/PaperExternalGiniTree.cpp
+++ src/PaperExternalGiniTree.cpp
@@ -388,7 +388,7 @@ bool round31C6FrozenOptionsValid(const SolveOptions& options,
         return false;
     }
     if (coarse_start != "off" &&
-        (!(hga_full || decoded_descent || (options.round59_simple_start && simple_start)) || causal != "off" || normalization != "proof" ||
+        (!(hga_full || decoded_descent || joint_insertion || (options.round59_simple_start && simple_start)) || causal != "off" || normalization != "proof" ||
          geometry_policy != "off" ||
          options.round40_c6_ub_geometry != "off" ||
          options.round41_static_segmented_gini != "off" ||
